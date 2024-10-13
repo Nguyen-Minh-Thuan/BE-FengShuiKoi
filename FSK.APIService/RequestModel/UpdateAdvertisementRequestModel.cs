8a078112142dccd35592b6e9a67ede9a8701ce0f
L4azy <hungnqse183854@fpt.edu.vn>
2024-10-14 02:26:31 +0700

    Update RequestModel

--- FSK.APIService/RequestModel/UpdateAdvertisementRequestModel.cs
+++ FSK.APIService/RequestModel/UpdateAdvertisementRequestModel.cs
@@ -3,7 +3,7 @@
     public class UpdateAdvertisementRequestModel
     {
         public int AdvertisementId { get; set; }
-        public int? AdsTypeId { get; set; }
+        public int AdsTypeId { get; set; }
         public int UserId { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
