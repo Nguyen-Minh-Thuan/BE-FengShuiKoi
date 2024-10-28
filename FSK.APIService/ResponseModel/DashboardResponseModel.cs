04cf8659c67f88316f7005790b28b728271b4812
L4azy <hungnqse183854@fpt.edu.vn>
2024-10-28 14:05:41 +0700

    More Data

---
+++ FSK.APIService/ResponseModel/DashboardResponseModel.cs
@@ -0,0 +1,12 @@
+﻿namespace FSK.APIService.ResponseModel
+{
+    public class DashboardResponseModel
+    {
+
+        public string date { get; set; }
+
+        public int content { get; set; }
+
+
+    }
+}
